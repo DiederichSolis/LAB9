cca613d3d2cce6953306254e9c8fec3e9844edef
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-05-23 07:38:11 -0600

    Intento de hacer un ciclo, hay falla ya que no finaliza

--- LAB9/Lab9parte1.asm
+++ LAB9/Lab9parte1.asm
@@ -22,7 +22,7 @@ ExitProcess proto,dwExitCode:dword
 	fmt db "%d", 0
 	msg1 db "El array contiene los siguientes elementos:",0AH, 0
 	msg2 db "Fin del array",0AH, 0
-	msg3 db "Ingrese el monto facturado: ",0AH,0
+	msg3 db "Ingrese el monoto facturado",0AH,0
 
 .code
     includelib libucrt.lib
@@ -34,3 +34,43 @@ ExitProcess proto,dwExitCode:dword
     extrn scanf:near
     extrn exit:near
 
+public main
+main proc
+	
+	mov ebx, offset arr
+	mov esi, sizeof	arr
+
+ingreso:
+	push offset msg3	
+	call printf			
+	
+	lea eax, [ebx]		; DIRECCIONAM. INDIRECTO: Cargar el valor del i-esimo elem de array a eax 
+	push eax			; Pasar valor a pila p/imprimir
+	push offset fmt		; Pasar formato 
+	call scanf
+	
+	add ebx, 4			; Decrementar "contador"
+	sub esi, 4			; Moverse al sig. elem. del array
+	cmp esi,0	 
+	jne ingreso
+
+
+label1:
+	mov eax, ebx		; DIRECCIONAM. INDIRECTO: Cargar el valor del i-esimo elem de array a eax 
+	push eax			; Pasar valor a pila p/imprimir
+	push offset fmt		; Pasar formato 
+	call printf
+
+	sub ebx, 4			; Decrementar "contador"
+	add esi, 4			; Moverse al sig. elem. del array
+	cmp ebx,0			; Aún hay elementos en el array?
+	jne label1			; Sí, entonces repetir proceso desde label1
+
+	push offset msg2	; Guarda en pila el valor de dirección inicial de msg2
+	call printf			; Llamado a printf
+
+	push 0
+    call exit           ; salir del programa
+
+main endp
+end
