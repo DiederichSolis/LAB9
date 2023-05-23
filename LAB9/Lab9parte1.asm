d22405d6e9523764d079213a6dcafe5f65f53fc1
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-05-22 21:38:45 -0600

    Se agrego encabezado

---
+++ LAB9/Lab9parte1.asm
@@ -0,0 +1,11 @@
+;Universidad del Valle de Guatemala
+;Diederich Solis 22952
+;Andy Fuentes 22944
+;
+;Descripcion: programa con informacion de 12 facturas de una empresa
+;donde por cada mes se le calcula el IVA
+;
+;Laboratorio 8 Parte II
+;
+;Catedratico: Roger Fuentes
+;Fecha de entrega: 24/05/2023
