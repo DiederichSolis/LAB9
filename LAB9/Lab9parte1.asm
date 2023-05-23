16f5225eb4d38903a0f9fde6d67dff13a588523f
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-05-23 15:00:15 -0600

    cambio de arrays

--- LAB9/Lab9parte1.asm
+++ LAB9/Lab9parte1.asm
@@ -11,66 +11,16 @@
 ;Fecha de entrega: 24/05/2023
 
 
-.386
-.model flat, stdcall, c
-.stack 4096
+.386 ; 
+.model flat, stdcall, c ; 
+.stack 4096 ; 
 
-ExitProcess proto,dwExitCode:dword
+ExitProcess proto,dwExitCode:dword ;
 
-.data
-	arr dword 0,0,0,0,0,0,0,0,0,0,0,0
-	fmt db "%d", 0
-	msg1 db "El array contiene los siguientes elementos:",0AH, 0
-	msg2 db "Fin del array",0AH, 0
-	msg3 db "Ingrese el monoto facturado",0AH,0
+.data 
 
-.code
-    includelib libucrt.lib
-    includelib legacy_stdio_definitions.lib
-    includelib libcmt.lib
-    includelib libvcruntime.lib
+;Arreglos
+	arrvalue dword 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	arrIva dword 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	arrMes BYTE 'Enero',0,'Febrero',0,'Marzo',0,'Abril',0,'Mayo',0,'Junio',0,'Julio',0,'Agosto',0,'Septiembre',0,'Octubre',0,'Noviembre',0,'Diciembre',0
 
-    extrn printf:near
-    extrn scanf:near
-    extrn exit:near
-
-public main
-main proc
-	
-	mov ebx, offset arr
-	mov esi, sizeof	arr
-
-ingreso:
-	push offset msg3	
-	call printf			
-	
-	lea eax, [ebx]		; DIRECCIONAM. INDIRECTO: Cargar el valor del i-esimo elem de array a eax 
-	push eax			; Pasar valor a pila p/imprimir
-	push offset fmt		; Pasar formato 
-	call scanf
-	
-	add ebx, 4			; Decrementar "contador"
-	sub esi, 4			; Moverse al sig. elem. del array
-	cmp esi,0	 
-	jne ingreso
-
-
-label1:
-	mov eax, ebx		; DIRECCIONAM. INDIRECTO: Cargar el valor del i-esimo elem de array a eax 
-	push eax			; Pasar valor a pila p/imprimir
-	push offset fmt		; Pasar formato 
-	call printf
-
-	sub ebx, 4			; Decrementar "contador"
-	add esi, 4			; Moverse al sig. elem. del array
-	cmp ebx,0			; Aún hay elementos en el array?
-	jne label1			; Sí, entonces repetir proceso desde label1
-
-	push offset msg2	; Guarda en pila el valor de dirección inicial de msg2
-	call printf			; Llamado a printf
-
-	push 0
-    call exit           ; salir del programa
-
-main endp
-end
