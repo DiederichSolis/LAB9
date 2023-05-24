a9f4777519b5e7846ee2e62ac1500d7c990f3b8b
DiederichSolis <105893797+DiederichSolis@users.noreply.github.com>
2023-05-24 09:42:54 -0600

    Se realizo el main

--- LAB9/Lab9parte1.asm
+++ LAB9/Lab9parte1.asm
@@ -22,5 +22,64 @@ ExitProcess proto,dwExitCode:dword ;
 ;Arreglos
 	arrvalue dword 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	arrIva dword 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	arrMes BYTE 'Enero',0,'Febrero',0,'Marzo',0,'Abril',0,'Mayo',0,'Junio',0,'Julio',0,'Agosto',0,'Septiembre',0,'Octubre',0,'Noviembre',0,'Diciembre',0
+	arrMes BYTE 'Ene23',0,'Feb23',0,'Mar23',0,'Abr23',0,'May23',0,'Jun23',0,'Jul23',0,'Ago23',0,'Sep23',0,'Oct23',0,'Nov23',0,'Dic23',0
 
+;Formatos
+	dato1 db "%d", 0
+	fmt db "%d ", 0AH,0
+	fmtPrueba BYTE "| %s | %d  | %d  |   %d   |", 0Ah, 0
+
+;Mensajes
+	msgTrue BYTE 'Cambiar de regimen a IVA general', 0Ah, 0
+	msgFalse BYTE 'Sigue siendo pequenio contribuyente', 0Ah, 0
+	msg_in byte "Ingresar monto facturado: ",0Ah, 0
+	msg_inNIT byte "Ingresar el NIT con el que facturara: ", 0
+	msgE db " ", 0ah, 0
+	msg2 db "|  MES  |    NIT   |  IVA  | FACTURADO | ",0AH, 0
+	msgx BYTE 'Monto facturado del mes: ', 0Ah,0
+
+;Variables	
+	divisor dword 20
+	suma dword 0
+	Total dword 0
+	NIT dword 0
+	IVA dword 0
+
+.code ; Sección de código
+
+;Librerias
+
+    includelib libucrt.lib ; 
+    includelib legacy_stdio_definitions.lib ; 
+    includelib libcmt.lib ; 
+    includelib libvcruntime.lib ; 
+
+	extrn printf:near
+	extrn scanf:near
+	extrn exit:near
+
+public main 
+main proc 
+
+	mov ebx, offset arrvalue
+	mov esi, sizeof arrvalue
+
+	push offset msg_inNIT					; Coloca la direc. de la cadena de caracteres en la pila
+	call printf								; Imprimir mensaje para ingreso de dato
+
+	add esp, 4								; Limpia la pila
+	lea eax, [ebp-4]						; Obtiene la dirección de la variable local
+	push eax								; Pone la dirección en la pila
+	push offset dato1						; Pone la dirección de la cadena de formato en la pila
+	call scanf								; Llama a la función scanf para leer el número ingresado
+	add esp, 8								; Limpia la pila
+	mov eax, [ebp-4]						; Mueve el número ingresado a eax
+	mov NIT, eax
+	push eax								; Pone el número en la pila
+
+	push offset msgE					; Coloca la direc. de la cadena de caracteres en la pila
+	call printf								; Imprimir mensaje para ingreso de dato
+	add esp, 8								; Limpia la pila
+
+	push offset msg_in						; Coloca la direc. de la cadena de caracteres en la pila
+	call printf								; Imprimir mensaje para ingreso de dato
